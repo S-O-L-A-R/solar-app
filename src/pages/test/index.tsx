189cac137b040464a62500a6f32b9d869a49b0a1
voraton.l <voraton@wongnai.com>
2019-09-14 23:22:46 +0700

    fix liff

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -13,9 +13,8 @@ export default () => {
 	return useObserver(() => (
 		<div>
 			<h1>LIFFFF</h1>
-			<h1>{store.userProfile && store.userProfile.value.data && store.userProfile.value.data.userId}</h1>
-			<h1>{store.userProfile && store.userProfile.value.data && store.userProfile.value.data.displayName}</h1>
-			<h1>{store.accessToken && store.accessToken.value.data && store.accessToken.value.data}</h1>
+			<h1>{store.userProfile && store.userProfile.value.data}</h1>
+			<h1>{store.accessToken && store.accessToken.value.data}</h1>
 			{store.error && <span>{store.error.message}/</span>}
 		</div>
 	))
