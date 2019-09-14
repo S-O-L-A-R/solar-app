7f0b7ae4820af87fae0ea043c2e39861f15b13d2
voraton.l <voraton@wongnai.com>
2019-09-14 22:47:47 +0700

    config liff endpoint

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useObserver } from 'mobx-react'
+import React, { useEffect } from 'react'
+import { useObserver, useAsObservableSource } from 'mobx-react'
 import mliffx from 'mliffx'
 
 export default () => {
+	const userProfile = useAsObservableSource(mliffx.userProfile)
+	const accessToken = useAsObservableSource(mliffx.accessToken)
+	const isInit = useAsObservableSource(mliffx.isInit)
+	const error = useAsObservableSource(mliffx.error)
+	useEffect(() => {
+		mliffx.getProfile()
+		mliffx.getAccessToken()
+	}, [isInit])
 	return useObserver(() => (
 		<div>
-			<h1>{mliffx.userProfile.value.data && mliffx.userProfile.value.data.userId}</h1>
-			<h1>{mliffx.userProfile.value.data && mliffx.userProfile.value.data.displayName}</h1>
-			<h1>{mliffx.accessToken.value.data && mliffx.accessToken.value.data}</h1>
+			<h1>{userProfile.value.data && userProfile.value.data.userId}</h1>
+			<h1>{userProfile.value.data && userProfile.value.data.displayName}</h1>
+			<h1>{accessToken.value.data && accessToken.value.data}</h1>
+			{error && <span>{error.message}/</span>}
 		</div>
 	))
 }
