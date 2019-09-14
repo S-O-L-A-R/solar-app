b9a1d38dfdcf03050a1f6e2fb4b9cc5dbb85e77b
voraton.l <voraton@wongnai.com>
2019-09-14 23:15:25 +0700

    fix liff test page

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,23 +1,22 @@
 import React, { useEffect } from 'react'
-import { useObserver, useAsObservableSource } from 'mobx-react'
+import { useObserver, useLocalStore } from 'mobx-react'
 import mliffx from 'mliffx'
 
 export default () => {
-	const userProfile = useAsObservableSource(mliffx.userProfile)
-	const accessToken = useAsObservableSource(mliffx.accessToken)
-	const isInit = useAsObservableSource(mliffx.isInit)
-	const error = useAsObservableSource(mliffx.error)
+	const store = useLocalStore(source => mliffx)
 	useEffect(() => {
-		mliffx.getProfile()
-		mliffx.getAccessToken()
-	}, [isInit])
+		if (store.isInit) {
+			store.getProfile()
+			store.getAccessToken()
+		}
+	}, [store])
 	return useObserver(() => (
 		<div>
 			<h1>LIFFFF</h1>
-			<h1>{userProfile.value.data && userProfile.value.data.userId}</h1>
-			<h1>{userProfile.value.data && userProfile.value.data.displayName}</h1>
-			<h1>{accessToken.value.data && accessToken.value.data}</h1>
-			{error && <span>{error.message}/</span>}
+			<h1>{store.userProfile && store.userProfile.value.data && store.userProfile.value.data.userId}</h1>
+			<h1>{store.userProfile && store.userProfile.value.data && store.userProfile.value.data.displayName}</h1>
+			<h1>{store.accessToken && store.accessToken.value.data && store.accessToken.value.data}</h1>
+			{store.error && <span>{store.error.message}/</span>}
 		</div>
 	))
 }
