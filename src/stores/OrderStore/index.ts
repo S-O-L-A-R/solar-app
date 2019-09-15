c29e147e35cbf6e7ec26be9bdaddd5153d4baf13
admin <phasin@wongnai.com>
2019-09-15 11:57:42 +0700

    use real userId

--- src/stores/OrderStore/index.ts
+++ src/stores/OrderStore/index.ts
@@ -1,26 +1,34 @@
-import { observable, action, computed } from 'mobx'
+import { observable, action, computed, runInAction } from 'mobx'
 import { Collection, Document } from 'firestorter'
 import RestaurantStore from 'stores/RestaurantStore'
 import { OrderItem } from 'types/Order'
 import { ORDER_STATUS } from 'common/constants'
 
-const userId = '@Gun'
-
 class OrderStore {
 	@observable
 	public ordersCollection?: Collection<Document<OrderItem>>
 
+	@observable
+	public user: any
+
 	@action
-	public setOrders() {
+	public async setOrders() {
 		if (!RestaurantStore.restaurant) return
 		this.ordersCollection = new Collection<Document<OrderItem>>(`${RestaurantStore.restaurant.path}/orders`)
+		if (liff.getProfile) {
+			const user = await liff.getProfile()
+			runInAction(() => {
+				this.user = user
+			})
+		}
 	}
 
 	@computed
 	get order() {
+		console.log(this.user, 'user')
 		if (!this.ordersCollection || !this.ordersCollection.hasDocs) return null
 		const order = this.ordersCollection.docs.find(({ data }: any) => {
-			return data.userId === userId && data.state === ORDER_STATUS.Pending
+			return this.user && data.userId === this.user.userId && data.state === ORDER_STATUS.Pending
 		})
 		if (!order) return null
 		return { ...order.data, id: order.id }
