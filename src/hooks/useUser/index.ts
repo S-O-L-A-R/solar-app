91ac35e88ce1bbca61e31a0168923c663ed7118c
voraton.l <voraton@wongnai.com>
2019-09-15 11:36:44 +0700

    fix

--- src/hooks/useUser/index.ts
+++ src/hooks/useUser/index.ts
@@ -3,9 +3,10 @@ import { Profile } from '@line/bot-sdk'
 
 export default function() {
 	const [user, setUser] = useState<Profile>({
-		userId: '',
-		pictureUrl: '',
-		displayName: '',
+		userId: 'U1632ccabdad8353298e60f4fdc9380e4',
+		pictureUrl:
+			'https://profile.line-scdn.net/0hJym161ylFVZQSD5bQKFqAWwNGzsnZhMeKC8OZSBAGDV0egFTbnoON3YcSGEpeAIDb31dY3wdT25_',
+		displayName: 'O1T_DEV - 怜雪 - R5I',
 		statusMessage: '',
 	})
 	useEffect(() => {
